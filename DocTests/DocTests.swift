7fb8f62ae47cde38e86cbd99d75e7c1e283d21d4
Balu <balumuchhal@outlook.com>
2025-04-22 15:29:14 +0530

    no message

---
+++ DocTests/DocTests.swift
@@ -0,0 +1,17 @@
+//
+//  DocTests.swift
+//  DocTests
+//
+//  Created by Seja Muchhal on 21/04/25.
+//
+
+import Testing
+@testable import Doc
+
+struct DocTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
